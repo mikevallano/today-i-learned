90ca7f680267b729897454602c0c3eff111bdaf7
mike vallano <mikevallano@gmail.com>
2016-05-04 11:56:40 -0500

    added specs to account for time zone in user methods

--- spec/features/reflections_feature_spec.rb
+++ spec/features/reflections_feature_spec.rb
@@ -3,12 +3,14 @@ require "rails_helper"
 RSpec.feature "Reflections feature spec", :type => :feature do
 
   feature "Users create, edit, update, view, and destroy reflections" do
-    let(:user) { FactoryGirl.create(:user) }
+    let(:user) { FactoryGirl.create(:user, time_zone: 'Central Time (US & Canada)') }
     let(:reflection) { FactoryGirl.create(:reflection, user: user) }
     let(:reflection_1_day_ago) { FactoryGirl.create(:reflection, created_at: 1.day.ago, user: user) }
     let(:reflection_2_days_ago) { FactoryGirl.create(:reflection, created_at: 2.days.ago, user: user) }
     let(:reflection_3_days_ago) { FactoryGirl.create(:reflection, created_at: 3.days.ago, user: user) }
     let(:reflection_5_days_ago) { FactoryGirl.create(:reflection, created_at: 5.days.ago, user: user) }
+    let(:reflection_today_utc) { FactoryGirl.create(:reflection, created_at: Time.current.at_beginning_of_day, user: user) }
+    let(:reflection_yesterday_utc) { FactoryGirl.create(:reflection, created_at: (Time.current.at_beginning_of_day - 1.day), user: user) }
 
     before(:each) do
       sign_in_user(user)
@@ -24,6 +26,37 @@ RSpec.feature "Reflections feature spec", :type => :feature do
       click_button "reflection_submit_button"
 
       expect(current_url).to eq(reflection_url(Reflection.last))
+    end
+
+    scenario "time_zone is defualt to UTC" do
+      reflection_today_utc
+
+      expect(reflection_today_utc.created_at.to_date).to eq(Time.current.at_beginning_of_day.in_time_zone("UTC").to_date)
+    end
+
+    scenario "time_zone is converted to user's time zone" do
+      reflection_today_utc
+
+      expect(reflection_today_utc.created_at.to_date).not_to eql(Time.current.at_beginning_of_day.in_time_zone(user.time_zone).to_date)
+    end
+
+    scenario "reflected_today is calculated from user's time zone" do
+      reflection_today_utc
+      visit root_path
+
+      #the homepage checks if the user has reflected 'today', which is relative to the user.
+      #reflection_today_utc was created in UTC, but the user's time_zone is 4 hours earlier, and therefore yesterday
+      expect(page).to have_content("Your last reflection was entered on #{user.last_reflection_date}")
+    end
+
+    scenario "current_streak is calculated from user's time zone" do
+      reflection_yesterday_utc
+      visit root_path
+
+      #current_streak checks if the user's last reflection was a day ago.
+      #reflection_yesterday_utc was yesterday in UTC, but the user's time zone is 4 hours earlier, therefore
+      #the user has not reflected within the past day, and the streak should be zero.
+      expect(page).to have_content("0 days")
     end
 
     scenario "shows current streak up to missing day" do
